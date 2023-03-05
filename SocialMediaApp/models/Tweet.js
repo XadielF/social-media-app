60c2a83a9d588d7a3cd2dfba5bd46fb3e4ed2c54
XadielF <xadiel.fdez@gmail.com>
2023-03-04 21:49:49 -0400

    Added the tweet schema for the db.

---
+++ SocialMediaApp/models/Tweet.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const tweetSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+module.exports = Tweet;
