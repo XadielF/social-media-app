17f4e544655a2851ac3e76a4e0819c8e14cf528e
XadielF <xadiel.fdez@gmail.com>
2023-03-13 18:10:58 -0400

    fixed react verisons.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/AboutPage";
 import ContactPage from "./components/ContactPage";
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
-      </Switch>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
