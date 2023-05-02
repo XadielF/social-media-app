a7e9ef9aeda73c89053bb63d20638a69b43f640d
XadielF <xadiel.fdez@gmail.com>
2023-05-01 20:20:40 -0400

    fixeing bg image

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./LandingPage.css";
-import backgroundImage from "../images/landingPagePic.jpg"
+import backgroundImage from "../images/landingPagePic.jpg";
 
 function LandingPage() {
   return (
@@ -13,15 +13,17 @@ function LandingPage() {
           <a href="Contact">Contact</a>
         </nav>
       </header>
-      <img src={backgroundImage} alt="background" className="backgroundd-image" />
+      <img
+        src={backgroundImage}
+        alt="background"
+        className="backgroundd-image"
+      />
       <main>
         <h2>Welcome to Twitter Clone</h2>
         <p>Join the conversation! Share your thoughts with the world.</p>
         <button className="btn">Get started</button>
       </main>
     </div>
-
-    
   );
 }
 
